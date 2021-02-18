ff2848a75722b61dfe4efe75e9cfc05056344e36
Damon Roberts <damonspencerroberts@gmail.com>
2021-02-18 13:50:26 +0000

    Seed the ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+json_string = open(url).read
+json_hash = JSON.parse(json_string, { symbolize_names: true })
+
+new_arr = json_hash[:drinks]
+new_arr.each do |drink|
+  Ingredient.create(name: drink[:strIngredient1])
+end
