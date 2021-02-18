d78119fabfda68dc5ce6d2dacb798fac69d8a95b
Damon Roberts <damonspencerroberts@gmail.com>
2021-02-18 14:11:25 +0000

    Add routes and controllers for cocktail and dose

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,9 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+  end
+
+  def create; end
+
+  def delete; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: %i[index create new show] do
+    member do
+      resources :doses, only: %i[new create]
+    end
+  end
+  delete 'doses/:id', to: 'doses#delete', as: 'delete_doses'
 end
