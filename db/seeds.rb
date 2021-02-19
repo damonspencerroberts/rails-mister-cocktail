3e8019835aad4734a4a6a712d6733857c01b83f5
Damon Roberts <damonspencerroberts@gmail.com>
2021-02-19 17:28:01 +0000

    Change seed to add doses

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,25 +8,25 @@
 require 'open-uri'
 require 'json'
 
-# create Ingredients
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-json_string = open(url).read
-json_hash = JSON.parse(json_string, { symbolize_names: true })
+# # create Ingredients
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# json_string = open(url).read
+# json_hash = JSON.parse(json_string, { symbolize_names: true })
 
-new_arr = json_hash[:drinks]
+# new_arr = json_hash[:drinks]
 
-# new_arr.each_with_index do |each, index|
-#   puts [each, index + 1]
+# # new_arr.each_with_index do |each, index|
+# #   puts [each, index + 1]
+# # end
+
+# new_arr.each do |drink|
+#   Ingredient.create(name: drink[:strIngredient1])
 # end
 
-new_arr.each do |drink|
-  Ingredient.create(name: drink[:strIngredient1])
-end
-
-new_ings = ['Mint', 'Ginger Beer', 'Redbull', 'Tonic', 'Ice', 'Campari', 'Olive']
-new_ings.each do |ing|
-  Ingredient.create(name: ing)
-end
+# new_ings = ['Mint', 'Ginger Beer', 'Redbull', 'Tonic', 'Ice', 'Campari', 'Olive']
+# new_ings.each do |ing|
+#   Ingredient.create(name: ing)
+# end
 
 # create Cocktails
 image_urls = [
@@ -45,7 +45,6 @@ new_image_url_files = image_urls.map do |url|
   URI.open(url)
 end
 
-p new_image_url_files
 
 init_tails = [
   'Mojito',
@@ -69,40 +68,40 @@ end
 
 # create doses
 # mojito
-Dose.create(description: '1.5 Ounces', cocktail_id: 22, ingredient_id: 1)
-Dose.create(description: '2 Teaspoons', cocktail_id: 22, ingredient_id: 25)
-Dose.create(description: 'A Little Bit', cocktail_id: 22, ingredient_id: 33)
-Dose.create(description: '1 Ounce', cocktail_id: 22, ingredient_id: 30)
-Dose.create(description: '6 Leaves', cocktail_id: 22, ingredient_id: 101)
+Dose.create(description: '1.5 Ounces', cocktail_id: 31, ingredient_id: 1 + 214)
+Dose.create(description: '2 Teaspoons', cocktail_id: 31, ingredient_id: 25 + 214)
+Dose.create(description: 'A Little Bit', cocktail_id: 31, ingredient_id: 33 + 214)
+Dose.create(description: '1 Ounce', cocktail_id: 31, ingredient_id: 30 + 214)
+Dose.create(description: '6 Leaves', cocktail_id: 31, ingredient_id: 101 + 214)
 # mule
-Dose.create(description: '1.5 Ounces', cocktail_id: 23, ingredient_id: 22)
-Dose.create(description: '4 Ounces', cocktail_id: 23, ingredient_id: 30)
-Dose.create(description: '0.5 Ounces', cocktail_id: 23, ingredient_id: 1)
+Dose.create(description: '1.5 Ounces', cocktail_id: 32, ingredient_id: 22 + 214)
+Dose.create(description: '4 Ounces', cocktail_id: 32, ingredient_id: 30 + 214)
+Dose.create(description: '0.5 Ounces', cocktail_id: 32, ingredient_id: 1 + 214)
 # white russian
-Dose.create(description: '1.67 Ounces', cocktail_id: 24, ingredient_id: 22)
-Dose.create(description: '1 Ounce', cocktail_id: 24, ingredient_id: 64)
-Dose.create(description: '0.67 Ounces', cocktail_id: 24, ingredient_id: 19)
+Dose.create(description: '1.67 Ounces', cocktail_id: 33, ingredient_id: 22 + 214)
+Dose.create(description: '1 Ounce', cocktail_id: 33, ingredient_id: 64 + 214)
+Dose.create(description: '0.67 Ounces', cocktail_id: 33, ingredient_id: 19 + 214)
 # Strawberry Daquiri
-Dose.create(description: '500 Grams', cocktail_id: 25, ingredient_id: 51)
-Dose.create(description: '1/2 Lime', cocktail_id: 25, ingredient_id: 56)
-Dose.create(description: '100 Ml', cocktail_id: 25, ingredient_id: 1)
+Dose.create(description: '500 Grams', cocktail_id: 34, ingredient_id: 51 + 214)
+Dose.create(description: '1/2 Lime', cocktail_id: 34, ingredient_id: 56 + 214)
+Dose.create(description: '100 Ml', cocktail_id: 34, ingredient_id: 1 + 214)
 # gin tonic
-Dose.create(description: '2 Ounces', cocktail_id: 26, ingredient_id: 3)
-Dose.create(description: '6 Ounces', cocktail_id: 26, ingredient_id: 104)
+Dose.create(description: '2 Ounces', cocktail_id: 35, ingredient_id: 3 + 214)
+Dose.create(description: '6 Ounces', cocktail_id: 35, ingredient_id: 104 + 214)
 # vodka Redbull
-Dose.create(description: '2 Ounces', cocktail_id: 27, ingredient_id: 22)
-Dose.create(description: '1 Can', cocktail_id: 27, ingredient_id: 103)
+Dose.create(description: '2 Ounces', cocktail_id: 36, ingredient_id: 22 + 214)
+Dose.create(description: '1 Can', cocktail_id: 36, ingredient_id: 103 + 214)
 # old fashioned
-Dose.create(description: '2 Ounces', cocktail_id: 28, ingredient_id: 20)
-Dose.create(description: '1 Cube', cocktail_id: 28, ingredient_id: 25)
-Dose.create(description: '1 Ounce', cocktail_id: 28, ingredient_id: 11)
-Dose.create(description: '1 Cube', cocktail_id: 28, ingredient_id: 105)
+Dose.create(description: '2 Ounces', cocktail_id: 37, ingredient_id: 20 + 214)
+Dose.create(description: '1 Cube', cocktail_id: 37, ingredient_id: 25 + 214)
+Dose.create(description: '1 Ounce', cocktail_id: 37, ingredient_id: 11 + 214)
+Dose.create(description: '1 Cube', cocktail_id: 37, ingredient_id: 105 + 214)
 # negroni
-Dose.create(description: '1 Ounce', cocktail_id: 29, ingredient_id: 3)
-Dose.create(description: '1 Ounce', cocktail_id: 29, ingredient_id: 15)
-Dose.create(description: '2 Ounces', cocktail_id: 29, ingredient_id: 106)
+Dose.create(description: '1 Ounce', cocktail_id: 38, ingredient_id: 3 + 214)
+Dose.create(description: '1 Ounce', cocktail_id: 38, ingredient_id: 15 + 214)
+Dose.create(description: '2 Ounces', cocktail_id: 38, ingredient_id: 106 + 214)
 
 # dry martini
-Dose.create(description: '1 Ounce', cocktail_id: 30, ingredient_id: 3)
-Dose.create(description: '1 Ounce', cocktail_id: 30, ingredient_id: 15)
-Dose.create(description: '1 Portion', cocktail_id: 30, ingredient_id: 107)
+Dose.create(description: '1 Ounce', cocktail_id: 39, ingredient_id: 3 + 214)
+Dose.create(description: '1 Ounce', cocktail_id: 39, ingredient_id: 15 + 214)
+Dose.create(description: '1 Portion', cocktail_id: 39, ingredient_id: 107 + 214)
