d78bf36e244cdc08619d17a7015dc56c104d4bed
Damon Roberts <damonspencerroberts@gmail.com>
2021-02-19 21:54:15 +0000

    Add update and delete to cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,10 +1,11 @@
 class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: %i[show edit update destroy]
+
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
     @doses = Dose.where('cocktail_id = ?', params[:id])
   end
 
@@ -21,7 +22,26 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @cocktail.update(cocktail_params)
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
   private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
 
   def cocktail_params
     params.require(:cocktail).permit(:name, :photo)
